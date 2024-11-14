d7b3472e1d5dc874e158afa3fb62f3fc056f93df
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2024-11-13 21:28:59 -0800

    Wrote ModuleIO interface class, created superstructure subsystem class

---
+++ src/main/java/org/frc6423/frc2024/subsystems/drive/module/Module.java
@@ -0,0 +1,5 @@
+package org.frc6423.frc2024.subsystems.drive.module;
+
+public class Module {
+    
+}
---
+++ src/main/java/org/frc6423/frc2024/subsystems/superstructure/Superstructure.java
@@ -0,0 +1,9 @@
+package org.frc6423.frc2024.subsystems.superstructure;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Superstructure extends SubsystemBase {
+
+
+    
+}
